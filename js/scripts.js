e62e3c2899049804153913de6f858070fdc97b9d
Kenny Grage & Jason Creighton <student@epicodus.com>
2015-09-14 12:24:00 -0700

    start multiple contact ability

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,39 @@
 $(document).ready(function() {
+  var newContact = { firstName: ["John"], lastName: ["Doe"], address: ["123 Main Street"] };
+
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
+
+    $("ul#contacts").empty();
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
     var inputtedAddress = $("input#new-address").val();
-    var newContact = { firstName: inputtedFirstName, lastName: inputtedLastName, address: inputtedAddress };
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
+    function addContact(inputtedFirstName, inputtedLastName, inputtedAddress){
+      newContact.firstName.push(inputtedFirstName);
+      newContact.lastName.push(inputtedLastName);
+      newContact.address.push(inputtedAddress);
+    };
 
+    addContact(inputtedFirstName, inputtedLastName, inputtedAddress);
+
+    for (var i = 0; i < newContact.firstName.length; i++){
+      $("ul#contacts").append("<li class='list-group-item'><span class='contact'>" + newContact.firstName[i] + " " + newContact.lastName[i] + "</span></li>");
+    }
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
     $("input#new-address").val("");
 
-    $(".contact").last().click(function() {  //by adding last, we don't add every elecment of Contact
-      $("#show-contact").show();
-      $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
-      $(".address").text(newContact.address);
-    });
+    for (var i = 0; i < newContact.firstName.length; i++) {
+      $(".contact").last().click(function() {  //by adding last, we don't add every elecment of Contact
+        $("#show-contact").show();
+        $("#show-contact h2").text(newContact.firstName[i] + " " + newContact.lastName[i]);
+        $(".first-name").text(newContact.firstName[i]);
+        $(".last-name").text(newContact.lastName[i]);
+        $(".address").text(newContact.address[i]);
+      });
+    }
   });
-
 });
