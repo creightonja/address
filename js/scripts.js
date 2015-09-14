0d02b9f6b796089a5e979ef1ed09ee151c06fa45
Kenny Grage & Jason Creighton <student@epicodus.com>
2015-09-14 14:31:44 -0700

    more jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-  var newContact = { firstName: ["John"], lastName: ["Doe"], address: ["123 Main Street"] };
+  var newContact = { firstName: ["John", "Mary"], lastName: ["Doe", "Jane"], address: ["123 Main Street", "567 State Street"]};
+  var ivalue = 0;
 
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
@@ -11,9 +12,11 @@ $(document).ready(function() {
     var inputtedAddress = $("input#new-address").val();
 
     function addContact(inputtedFirstName, inputtedLastName, inputtedAddress){
+      ivalue++;
       newContact.firstName.push(inputtedFirstName);
       newContact.lastName.push(inputtedLastName);
       newContact.address.push(inputtedAddress);
+      //newContact.ivalue.push(ivalue);
     };
 
     addContact(inputtedFirstName, inputtedLastName, inputtedAddress);
@@ -26,14 +29,15 @@ $(document).ready(function() {
     $("input#new-last-name").val("");
     $("input#new-address").val("");
 
-    for (var i = 0; i < newContact.firstName.length; i++) {
-      $(".contact").last().click(function() {  //by adding last, we don't add every elecment of Contact
-        $("#show-contact").show();
-        $("#show-contact h2").text(newContact.firstName[i] + " " + newContact.lastName[i]);
-        $(".first-name").text(newContact.firstName[i]);
-        $(".last-name").text(newContact.lastName[i]);
-        $(".address").text(newContact.address[i]);
-      });
-    }
+      for (var key in newContact) {
+        $(".contact").last().click(function() {  //by adding last, we don't add every elecment of Contact
+          $("#show-contact").show();
+          $("#show-contact h2").text($(this).attr(firstName + " " + $(this).attr(lastName));
+          $(".first-name").text($(this).attr(firstName);
+          $(".last-name").text($(this).attr(lastName);
+          $(".address").text($(this).attr(address);
+        });
+      }
+
   });
 });
